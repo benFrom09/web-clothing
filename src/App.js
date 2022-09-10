e57bf3cfa274f18caca0b3a24906f43d94e8633d
benFrom09 <ben09.dev@gmail.com>
2022-09-10 12:18:59 +0200

    set up basic component architecture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 const App = () => {
   return (
-    <div className="App">
-     Hello world
+    <div className="categories-container">
+      <div className="category-container">
+        {/*<img/>*/}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*<img/>*/}
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*<img/>*/}
+        <div className="category-body-container">
+          <h2>sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*<img/>*/}
+        <div className="category-body-container">
+          <h2>womens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/*<img/>*/}
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
     </div>
   );
 }
