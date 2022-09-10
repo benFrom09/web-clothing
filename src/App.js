061a19ba8308916af63a33e721ce7820b22159f6
benFrom09 <ben09.dev@gmail.com>
2022-09-10 13:10:48 +0200

    refactor App before styling

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ]
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/*<img/>*/}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img/>*/}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img/>*/}
-        <div className="category-body-container">
-          <h2>sneakers</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img/>*/}
-        <div className="category-body-container">
-          <h2>womens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img/>*/}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop now</p>
-        </div>
-      </div>
+      {categories.map(({ title,id }) => {
+        return (
+          <div key={id} className="category-container">
+            <div className="background-image"></div>
+            {/*<img/>*/}
+            <div className="category-body-container">
+              <h2>{ title }</h2>
+              <p>Shop now</p>
+            </div>
+          </div>
+        )
+
+      })}
+
     </div>
   );
 }
